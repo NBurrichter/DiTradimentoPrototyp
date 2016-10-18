e3bcd65dfe5c8e52a08b1e431dafba7833476839
Niklas Burrichter <niklas.burrichter@stud.h-da.de>
2016-10-18 18:40:51 +0200

    MManager update

--- MultiplayerPrototyp/MultiplayerPrototype/Assets/ServerManager.cs
+++ MultiplayerPrototyp/MultiplayerPrototype/Assets/ServerManager.cs
@@ -8,7 +8,7 @@ public class ServerManager : MonoBehaviour {
     {
         0,
         0,
-        0,
+        //0,
         1,
         2
     };
@@ -18,20 +18,20 @@ public class ServerManager : MonoBehaviour {
         {
             Debug.Log(professionList[i]);
         }
-        /*Debug.Log("Set Profession");
+        Debug.Log(professionList.Count);
         //Give Players their profesion
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject player in players)
         {
-            int listposition = Random.Range(0, professionList.Count + 1);
-            for (int i = 0; i <= professionList.Count; i++)
+            int listposition = Random.Range(0, professionList.Count);
+            for (int i = 0; i < professionList.Count; i++)
             {
                 Debug.Log(professionList[i]);
             }
             Debug.Log("|");
             player.GetComponent<Player>().profession = professionList[listposition];
             professionList.RemoveAt(listposition);
-        }*/
+        }
     }
 	
 }
